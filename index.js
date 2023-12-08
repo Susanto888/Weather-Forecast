3dada7cf03fad35cd38eef4061c339360640dbc5
Susanto888 <lianq96@gmail.com>
2023-12-08 13:07:08 +0700

    add .env to gitignore

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import { fileURLToPath } from "url";
 const app = express();
 const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const pathToEnvFile = __dirname + "/api.env"
+const pathToEnvFile = __dirname + "/.env"
 
 dotenv.config({ path: pathToEnvFile})
 app.use(express.static("public"));
